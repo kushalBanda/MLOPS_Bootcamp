dad8c10d9e41c53a30f2b581854e07b1316fe34d
Kushal Banda <kushalbanda265@gmail.com>
2025-01-14 20:07:20 +0530

    Folders Added

--- Project/1. Data Science/main.py
+++ Project/1. Data Science/main.py
@@ -1,6 +1,8 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingsPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -16,8 +18,30 @@ except Exception as e:
 STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
-    data_ingestion = DataValidationTrainingsPipeline()
-    data_ingestion.initiate_data_validation()
+    data_validation = DataValidationTrainingsPipeline()
+    data_validation.initiate_data_validation()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=================x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.initiate_data_transformation()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=================x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    model_training = ModelTrainerTrainingPipeline()
+    model_training.initiate_model_training()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=================x")
 
 except Exception as e:
